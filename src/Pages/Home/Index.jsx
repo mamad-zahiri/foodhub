84f3f150528058b4bdb10abbaafa39a2078913be
Mohamad Zahiri <mamadzhri@gmail.com>
2023-07-22 11:55:17 +0330

    Add home page about us
    a little margin fix in home page too

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -106,7 +106,7 @@ const Home = () => {
             <Hr />
 
             {/* payDay promo: start */}
-            <SectionFlex className="mt-14">
+            <SectionFlex className="my-14">
                 <div className="mb-5 max-w-[500px] px-5">
                     <div className="flex items-center justify-between rounded-md bg-slate-800 bg-opacity-50 shadow-lg">
                         <div className="flex w-3/5 flex-col items-start justify-center gap-y-2 px-4 py-4 sm:py-8">
@@ -136,6 +136,45 @@ const Home = () => {
                 </div>
             </SectionFlex>
             {/* payDay promo: end */}
+
+            <Hr />
+
+            {/* about us: start */}
+            <SectionFlex className="my-14 gap-x-8 px-4 lg:px-0">
+                <figure className="mb-7 flex w-full lg:mb-0 lg:w-1/2">
+                    <img src={getImageUrl("home-our-brand.jpg")} alt="foodhub brand" className="max-w-full rotate-180 rounded-2xl" />
+                </figure>
+
+                <div className="flex w-full flex-col items-center justify-center gap-y-7 text-center sm:items-start sm:text-left lg:w-1/2">
+                    <h4 className="font-[Staatliches] text-5xl text-white">FIND YOUR BEST TASTED FOOD & DRINK JUST IN ONE PLACE</h4>
+                    <hr className="h-0.5 w-full border-yellow-500 bg-yellow-500 md:w-1/4" />
+                    <p className="text-sm text-slate-300">
+                        This is FoodHub! Welcome to our awesome fast food joint. We're all about serving up mouth-watering burgers, and
+                        yummy snacks to satisfy your cravings. We take pride in using only the freshest ingredients and making sure that our
+                        food is prepared with love. So, come on over and kick back in our chill atmosphere while you enjoy some seriously
+                        good eats. We can't wait to see you!
+                    </p>
+                    <ul
+                        className="flex flex-row flex-wrap gap-y-2 text-sm text-white
+                        [&>li>img]:mr-2 [&>li>img]:h-auto [&>li>img]:w-4 [&>li]:flex [&>li]:w-1/2"
+                    >
+                        <li>
+                            <img src={getImageUrl("check.png")} alt="check" /> Best Price
+                        </li>
+                        <li>
+                            <img src={getImageUrl("check.png")} alt="check" /> Best Service
+                        </li>
+                        <li>
+                            <img src={getImageUrl("check.png")} alt="check" /> Fresh Ingredient
+                        </li>
+                        <li>
+                            <img src={getImageUrl("check.png")} alt="check" /> Health Protocol
+                        </li>
+                    </ul>
+                    <button className="rounded-sm bg-yellow-500 px-7 py-3 font-normal">About Us</button>
+                </div>
+            </SectionFlex>
+            {/* about us: end */}
         </>
     )
 }
