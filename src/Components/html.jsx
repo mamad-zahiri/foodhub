a92079560ce1c199fce23cdb18a0ef1a8cca870c
Mohamad Zahiri <mamadzhri@gmail.com>
2023-07-22 08:56:47 +0330

    Add home page "payDay promo"
    files:
        src/Components/html.jsx:
                Add BuyOneBtn Component

--- src/Components/html.jsx
+++ src/Components/html.jsx
@@ -5,4 +5,10 @@ const SectionFlex = (props) => {
 
 const Hr = () => <hr className="mx-auto max-w-sm border-gray-800 md:max-w-xl lg:max-w-4xl" />
 
-export { SectionFlex, Hr }
+const ByOneBtn = (props) => (
+    <a href={props.href} className={"mt-2 rounded-full bg-slate-200 px-4 py-1 text-sm text-gray-950 " + props.className}>
+        {props.children ? props.children : "Buy One"}
+    </a>
+)
+
+export { SectionFlex, Hr, ByOneBtn }
--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,5 +1,5 @@
 import { getImageUrl } from "../../utils/helper"
-import { SectionFlex, Hr } from "../../Components/html"
+import { SectionFlex, Hr, ByOneBtn } from "../../Components/html"
 
 const Home = () => {
     return (
@@ -108,6 +108,40 @@ const Home = () => {
                 </div>
             </SectionFlex>
             {/* categories: end */}
+
+            <Hr />
+
+            {/* payDay promo: start */}
+            <SectionFlex className="mt-14">
+                <div className="mb-5 max-w-[500px] px-5">
+                    <div className="flex items-center justify-between rounded-md bg-slate-800 bg-opacity-50 shadow-lg">
+                        <div className="flex w-3/5 flex-col items-start justify-center gap-y-2 px-4 py-4 sm:py-8">
+                            <p className="text-[11px] text-yellow-400 sm:text-xs">PayDay Promo</p>
+                            <h4 className="font-[Staatliches] text-3xl text-white">GET 10% DISCOUNT ON PAYDAY WEEK</h4>
+                            <h4 className="text-xs text-slate-400">Get 10% discount on our bigget Burger on this week.</h4>
+                            <ByOneBtn className="bg-yellow-400 text-black" href="" />
+                        </div>
+                        <figure className="flex w-2/5 items-center justify-center">
+                            <img src={getImageUrl("home-off-hamburger.png")} alt="cola drink off" className="w-44 drop-shadow-dark" />
+                        </figure>
+                    </div>
+                </div>
+
+                <div className="mb-5 max-w-[500px] px-5">
+                    <div className="flex items-center justify-between rounded-md bg-slate-800 bg-opacity-50 shadow-lg">
+                        <div className="flex w-3/5 flex-col items-start justify-center gap-y-2 px-4 py-4 sm:py-8">
+                            <p className="text-[11px] text-yellow-400 sm:text-xs">PayDay Promo</p>
+                            <h4 className="font-[Staatliches] text-3xl text-white">BUY 1 COKE GET MORE 1 FREE COKE</h4>
+                            <h4 className="text-xs text-slate-400">Enjoy having free drink with your friend with us.</h4>
+                            <ByOneBtn className="bg-yellow-400 text-black" href="" />
+                        </div>
+                        <figure className="flex w-2/5 items-center justify-center">
+                            <img src={getImageUrl("home-off-drink.png")} alt="cola drink off" className="w-44 drop-shadow-dark" />
+                        </figure>
+                    </div>
+                </div>
+            </SectionFlex>
+            {/* payDay promo: end */}
         </>
     )
 }
